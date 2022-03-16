77e3c8c27e14bed8acfcc01a1fa44bc153186077
Md Asaduzaman <asadasad9282@gmail.com>
2022-03-16 22:32:02 +0600

    fetch-api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
-
-
-
-
 function App() {
+  return (
+    <div>
+      <LoadCountries></LoadCountries>
+    </div>
+  )
+}
 
+function LoadCountries() {
+
+  const [countries, setCountries] = useState([])
+  useEffect(() => {
+    fetch('https://restcountries.com/v3.1/all')
+      .then(res => res.json())
+      .then(data => setCountries(data))
+  }, [])
+
+  return (
+    <div>
+      <h1>vesiting every country of the world</h1>
+      <h3>All contries:{countries.length}</h3>
+    </div>
+  )
 
 
 }
+
 
 export default App;
